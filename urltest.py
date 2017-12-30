15b802bb1603f7a10e8123f7ab65981a3fad704b
kenta naito <kentanaito@kenta-no-MacBook-Pro.local>
2017-12-31 00:45:39 +0900

    画像取得コード追記

--- urltest.py
+++ urltest.py
@@ -1,1 +1,8 @@
-import
+import urllib.request
+
+url = "http://kawasaki-fujimi.com/info/2017/img0731/03-02.jpg"
+imagefile = "lacrosse.jpg"
+
+urllib.request.urlretrieve(url,imagefile)
+
+print("File Saved")
